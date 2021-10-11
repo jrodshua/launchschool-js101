2848faaf22ca1bb271fe541a2b8585f41d3dfc24
Josh Rodriguez <jrodshua@gmail.com>
2021-10-11 15:09:58 -0700

    lesson 2 calculator

--- Lesson_2/calculator.js
+++ Lesson_2/calculator.js
@@ -6,16 +6,50 @@ const readline = require('readline-sync');
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 
-console.log('Welcome to Calculator!');
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log(`What's the first number?`);
+prompt(`Welcome to Calculator!`);
+
+// console.log(`What's the first number?`);
+// let number1 = readline.question();
+
+// console.log(`What's the second number?`);
+// let number2 = readline.question();
+
+// console.log(`What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
+// let operation = readline.question();
+
+prompt(`What's the first number?`);
 let number1 = readline.question();
 
-console.log(`What's the second number?`);
+while (invalidNumber(number1)) {
+  prompt(`Hmm... that doesn't look like a valid number.`);
+  number1 = readline.question();
+}
+
+prompt(`What's the second number?`);
 let number2 = readline.question();
 
-console.log(`What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
+while (invalidNumber(number2)) {
+  prompt(`Hmm... that doesn't look like a valid number.`);
+  number2 = readline.question();
+}
+
+prompt(
+  `What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`
+);
 let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt(`Must choose 1, 2, 3 or 4`);
+  operation = readline.question();
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
 
 let output;
 if (operation === '1') {
